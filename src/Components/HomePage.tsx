bc7e6213578b04b7bff4809732ffa932f7af608e
Pritumi Patel <pritumi8@yorku.ca>
2024-11-11 22:04:39 -0500

    created the right side mail component

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,10 +1,15 @@
 // HomePage consists of the header, 
 
 import Header from "./Header"
+import Mail from "./Mail"
 
 const HomePage = () =>{
     return(
-        <Header></Header>
+        <>
+            <Header></Header>
+            <Mail></Mail>
+        
+        </>
     )
 }
 export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {colors: {
+      bgColor: "#0A192F",
+      primaryColor: "#64FFDA",
+      textColor: "#8892B0",
+    },},
     screens: {
 
       'xs': '476px',
@@ -20,11 +24,6 @@ module.exports = {
       'md-mx': {'max': '767px'},
       'sm-mx': {'max': '639px'},
       'xs-mx': {'max': '475px'},
-    },
-    colors: {
-      bgColor: "#0A192F",
-      primaryColor: "#64FFDA",
-      textColor: "#8892B0",
     },
   },
   plugins: [],
