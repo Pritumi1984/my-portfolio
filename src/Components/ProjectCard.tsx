5a4be631375f8fcf79bac12c6516988847ebc733
Pritumi Patel <pritumi.patel1984@gmail.com>
2024-11-13 11:47:00 -0500

    Created the project modal

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -1,26 +1,47 @@
-import { Badge, Button, Image, Card, Text, Group } from "@mantine/core";
+import { Badge, Button, Image, Card, Text, Group, Indicator } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import FullProjectModal from "./FullProjectModal";
 
-const ProjectCard=()=>{
+const ProjectCard=(props:any)=>{
+    const [opened, { open, close }] = useDisclosure(false);
     return(
-        <Card className="w-[360px]" shadow="sm" padding="lg" radius="md" withBorder>
-      <Card.Section>
-        <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png" height={160} alt="Norway"/>
+        <>
+        <Card onClick={open} className="w-[310px] mb-5 cursor-pointer transition-transform duration-300 ease-in-out
+         !bg-bgColor hover:!scale-[1.02] hover:!shadow-[0_0px_10px_1px_#64FFDA] !border-primaryColor 
+         border-2" shadow="lg" padding="sm" radius="lg" withBorder>
+      <Card.Section className="p-3">
+        <Image className="!rounded-xl !shadow-[0_0px_5px_0px_#64FFDA]" 
+            src={props.image}  
+            alt={props.image}
+        />
       </Card.Section>
 
-      <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>Norway Fjord Adventures</Text>
-        <Badge color="pink">On Sale</Badge>
+      <Group justify="space-between" mt="xs" mb="xs">
+        <Text className="!text-2xl !font-bold !text-white gap-2 flex items-center">{props.title} {props.live===true &&
+            <Badge variant="outline" color="red" rightSection={<Indicator color="red" position="middle-end" size={7} processing></Indicator>}>
+                Live 
+            </Badge>}
+        </Text>
       </Group>
 
-      <Text size="sm" c="dimmed">
-        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-        activities on and around the fjords of Norway
+      <Group mb="xs">
+        {props.technologies.map((tech:string, index:number)=> index<3 &&
+            <Badge key={index} variant="light" color="#64FFDA" size="md">{tech}</Badge>
+        )}
+      </Group>
+
+      <Text className="!text-justify" lineClamp={5} size="sm" c="dimmed">
+        {props.desc}
       </Text>
 
-      <Button color="blue" fullWidth mt="md" radius="md">
-        Book classic tour now
+      <Button onClick={open} className="!bg-primaryColor !text-bgColor" fullWidth mt="md" radius="md">
+        Show More
       </Button>
     </Card>
+    <FullProjectModal opened={opened} close={close} title={props.title}
+            desc={props.desc} image={props.image} live={props.live} link={props.link}
+            github={props.github} technologies={props.technologies}/>
+    </>
     )
 }
 
--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,13 +1,16 @@
 import ProjectCard from "./ProjectCard";
+import { ProjectInfo } from "../User";
 
 const Projects=()=>{
     return(
       <div className="px-16 mx-20 my-5 font-mono">
         <h1 className="text-4xl mb-5 font-bold text-white"><span className="text-primaryColor">02.&nbsp;</span>Projects</h1>
-        <div className="flex justify-between gap-4">
-          <ProjectCard></ProjectCard>
-          <ProjectCard></ProjectCard>
-          <ProjectCard></ProjectCard>
+        <div className="flex flex-wrap justify-between gap-4">
+          {
+            ProjectInfo.map((project:any, index:number)=><ProjectCard key={index} title={project.title}
+            desc={project.desc} image={project.image} live={project.live} link={project.link}
+            github={project.github} technologies={project.technologies}/>)
+          }
         </div>
       </div>
     );
