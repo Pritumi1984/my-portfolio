911429d7c3eca1ce0a7c209ba5d13cbf79fb02af
Pritumi Patel <pritumi.patel1984@gmail.com>
2025-02-09 12:11:04 -0500

    Added form validations and imported database

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import FloatingInput from "./FloatingInput";
 import { Button } from "@mantine/core";
-import { IconArrowRight } from "@tabler/icons-react";
-
+import { IconArrowRight, IconMailHeart } from "@tabler/icons-react";
+import { validateForm } from "./Validation";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../Firebase";
+import toast from "react-hot-toast";
 
 const Contact =() => {
 
@@ -16,22 +19,45 @@ const Contact =() => {
   const [formData, setFormData] = useState<{ [key: string]: string }>(form);
   const [formError, setFormError] = useState<{ [key: string]: string }>(form);
 
-  return (
-    <div className="px-16 ml-16 md-mx:px-6 !my-10 font-mono" id="Contact">
-       <h1 className="text-4xl sm-mx:text-3xl mt-20 xs-mx:text-2xl mb-10 font-bold text-center text-white"><span className="text-primaryColor">05.&nbsp;</span>Contact</h1>
-       <div data-aos="flip-left" data-aos-duration="800" className=" w-[70%] shadow-[0_0_10px_0_#64FFDA50] m-auto flex flex-col gap-6 border border-primaryColor rounded-3xl p-8 ">
-          <div className="text-2xl text-white font-semibold">Let's Connect</div>
-          <FloatingInput id="name" name="Name" value={formData.name}></FloatingInput>
-          <FloatingInput id="email" name="Email" value={formData.email}></FloatingInput>
-          <FloatingInput id="phone" name="Phone Number" value={formData.phone}></FloatingInput>
-          <FloatingInput id="message" name="Message" value={formData.message}></FloatingInput>
-          <Button fullWidth rightSection={<IconArrowRight size={20} />}
+  const handleChange = (id: string, value: string) => {
+    setFormData({ ...formData, [id]: value });
+    setFormError({ ...formError, [id]: validateForm(id, value)})
+  }
+  const handleSubmit=async()=>{
+    let valid=true;
+    let newFormError:{[key:string]:string}={};
+    for(let key in formData){
+        const error=validateForm(key, formData[key]);
+        if(error.length>0){
+            newFormError[key]=error;
+            valid=false;
+        }
+    }
+    setFormError(newFormError);
+    if(valid){
+        setFormData(form);
+        toast.success('Submitted Successfully!', {duration:4000});
+        await addDoc(collection(db, "portfolio"), formData);
+    }
+    else{
+        toast.error('Some error occurred!', {duration:4000})
+    }
+  }
+
+  return( 
+    <div className="px-16 md-mx:px-8 sm-mx:px-4 mx-20 lg-mx:mx-10 md-mx:mx-0   my-10  font-mono" id="Contact">
+        <h1 className="text-4xl  sm-mx:text-3xl xs-mx:text-2xl mb-10 font-bold text-center text-white"><span className="text-primaryColor">05.&nbsp;</span>Contact</h1>
+        <div data-aos="flip-left" data-aos-duration="800" className="w-[70%] lg-mx:w-full shadow-[0_0_10px_0_#64FFDA50] m-auto flex flex-col gap-6 border border-primaryColor p-8 rounded-3xl sm-mx:p-4">
+            <div className=" text-3xl flex gap-2 items-center text-white font-semibold sm-mx:text-2xl xs-mx:text-xl">Let's Connect...<IconMailHeart className="w-8 gap-8 text-primaryColor h-8 sm-mx:w-7 sm-mx:h-7" /></div>
+            <FloatingInput id="name" name="Name" value={formData.name} handleChange={handleChange}  error={formError.name}/>
+            <FloatingInput id="email" name="Email" value={formData.email} handleChange={handleChange}  error={formError.email}/>
+            <FloatingInput id="phone" name="Phone Number" value={formData.phone} handleChange={handleChange}  error={formError.phone}/>
+            <FloatingInput id="message" name="Message" value={formData.message} handleChange={handleChange}  error={formError.message}/>
+            <Button fullWidth onClick={handleSubmit} rightSection={<IconArrowRight size={20} />}
                 className="!text-bgColor !font-bold " variant="filled" radius="lg" color="#64FFDA">Send</Button>
-        
-       </div>
-    
+        </div>
     </div>
-  );
+  )
 }
 
 export default Contact;
